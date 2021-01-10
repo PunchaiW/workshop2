526b0bff4383b116ae792c7073ffccdf9e90f35a
PunchaiW <punchai.w@ku.th>
2021-01-10 15:39:50 +0700

    declare many variable in one line

---
+++ variable/name.py
@@ -0,0 +1,11 @@
+# ตัวแปรที่สามารถประกาศได้
+myvar = "A"
+my_var = "A"
+_my_var = "A"
+myVar = "A"
+MYVAR = "A"
+myvar2 = "A"
+# ตัวแปรที่ไม่สามารถประกาศได้
+# 2myvar = "A"
+# my-var = "A"
+# my var = "A"
