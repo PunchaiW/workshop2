7e2778c0a48d45d106cc9ca342fca306f7f92b0a
PunchaiW <punchai.w@ku.th>
2021-01-10 14:15:57 +0700

    add create , get_type , single_or_double

---
+++ variable/create.py
@@ -0,0 +1,10 @@
+name = "Missing potato"
+age = 1
+weight = 10
+growOnDirt = False
+growOnCrate = True
+print(name)
+print(age)
+print(weight)
+print(growOnDirt)
+print(growOnCrate)
---
+++ variable/get_type.py
@@ -0,0 +1,10 @@
+name = "Missing potato"
+age = 1
+weight = 10
+growOnDirt = False
+growOnCrate = True
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(growOnDirt))
+print(type(growOnCrate))
