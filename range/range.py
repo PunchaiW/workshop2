a19a4f1e358919c275bbdf8c028abd7f8bd91e67
PunchaiW <punchai.w@ku.th>
2021-02-06 16:18:32 +0700

    Learn range

---
+++ range/range.py
@@ -0,0 +1,21 @@
+number_list = range(6)
+
+print(number_list)
+
+# Output: [0,1,2,3,4,5]
+
+print("--------------")
+
+number_list = range(2, 6)
+
+print(number_list)
+
+# Output: [2,3,4,5]
+
+print("--------------")
+
+number_list = range(3, 37, 3)
+
+print(number_list)
+
+# Output: [3,6,9,12,15,18,21,24,27,30,33,36]
